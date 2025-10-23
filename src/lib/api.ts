151d8e68859fa29d7fb2118a0b1054b09d5dac09
Kushalchavan <kushalchavan2002@gmail.com>
2025-10-23 23:37:51 +0530

    add small changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,19 +1,3 @@
-import { Coin } from "@/types/coins";
-
-// export async function getCoins(): Promise<Coin[]> {
-//    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/coins`, {
-//     next: { revalidate: 60 }, // cache for 60s
-//   });
-
-
-//   if (!response.ok) {
-//     throw new Error("Failed to fetch coins");
-//   }
-
-//   return response.json();
-// }
-
-
 export async function getCoins() {
   const res = await fetch("/api/coins", { next: { revalidate: 60 } });
 
--- src/types/coins.ts
+++
@@ -1,6 +0,0 @@
-export interface Coin {
-  id: string;
-  name: string;
-  price_usd: number;
-  type_is_crypto: number;
-}
